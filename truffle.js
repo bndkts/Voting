161c81b0385baa8bf3d6a23aaf42dcf9f497b48b
Temitope Alabi <temitopealabi@Temitopes-MacBook-Pro.local>
2017-12-28 15:43:58 -0600

    first commit

---
+++ truffle.js
@@ -0,0 +1,24 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+     ropsten:  {
+     network_id: 3,
+     host: "localhost",
+     port:  8545,
+     gas:   470000
+
+}
+  },
+   rpc: {
+ host: 'localhost',
+ post:8080
+   }
+};
